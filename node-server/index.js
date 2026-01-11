a5614dc130ca6c1b31e806a57f572404ce1b4577
AkshatJha21 <akshatj2134@gmail.com>
2026-01-11 23:19:41 +0530

    Http server without express

--- node-server/index.js
+++ node-server/index.js
@@ -1,9 +1,23 @@
-import { add, pathBatao, fileSystum } from "./utils.js";
+// import { add, pathBatao, fileSystum } from "./utils.js";
+import http from "http";
 
 // JS runtime
 // Built on v8
 // Uses event driven non-blocking i/o
 
-console.log(add(2, 3));
-pathBatao();
-fileSystum();
+// console.log(add(2, 3));
+// pathBatao();
+// fileSystum();
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write("Home");
+    } else if (req.url === "/users") {
+        res.write(JSON.stringify([{ id: 1, name: "Akshat" }]));
+    }
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
